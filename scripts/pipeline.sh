b841171cb9757f599808c526523eeb5943c0e18e
Laura Díaz Regalado <lauradiazregalado@gmail.com>
2019-11-19 13:17:58 +0100

    DONE

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,4 +1,4 @@
-echo "COMIENZA EL SCRIPT"
+echo "ANLYSING HAS STARTED"
 #Download all the files specified in data/urls
 for url in $(cat data/urls) #TODO
 do
@@ -7,11 +7,12 @@ done
 
 # Download the contaminants fasta file, and uncompress it
 bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
-
+echo "ALL NECESSARY DATA HAVE BEEN DOWNLOADED"
 # Index the contaminants file
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 # Merge the samples into a single file
+echo "RUNING FASTQ"
 for sid in $(ls data/*.fastq.gz | cut -d"-" -f1 | sed 's:data/::' | sort | uniq)
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
@@ -24,6 +25,7 @@ mkdir -p out/trimmed
 for sid in $(ls out/merged/*fastq.gz | cut -d"." -f1 | sed 's:out/merged/::' )
 do
 	cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/$sid.trimmed.fastq.gz  out/merged/$sid.fastq.gz > log/cutadapt/$sid.log
+	echo "CUTADAPT DONE FOR $sid"
 done
 #TODO: run STAR for all trimmed files
 echo "RUNNING STAR ALIGNMENT"
@@ -32,6 +34,8 @@ do
 	sid=$(echo $(basename $fname .trimmed.fastq.gz))
 	mkdir -p out/star/$sid/
     	STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn $fname --readFilesCommand zcat --outFileNamePrefix out/star/$sid/
+	echo "ALIGNEMENT DONE FOR $sid"
+echo "ANALYSING FOR $sid HAVE FINISHED"
 done
 # TODO: create a log file containing information from cutadapt and star logs
 # (this should be a single log file, and information should be *appended* to it on each run)
