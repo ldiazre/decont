7293d2d6b5cbef170c168fb8a2a59af1fb8b6000
Laura Díaz Regalado <lauradiazregalado@gmail.com>
2019-11-19 12:50:27 +0100

    STILL IN PROCESS

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,30 +1,30 @@
 echo "COMIENZA EL SCRIPT"
 #Download all the files specified in data/urls
-#for url in $(cat data/urls) #TODO
-#do
-#    bash scripts/download.sh $url data
-#done
+for url in $(cat data/urls) #TODO
+do
+    bash scripts/download.sh $url data
+done
 
 # Download the contaminants fasta file, and uncompress it
-#bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
+bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
 
 # Index the contaminants file
-#bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
+bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 # Merge the samples into a single file
-#for sid in $(ls data/*.fastq.gz | cut -d"-" -f1 | sed 's:data/::' | sort | uniq)
-#do
-#    bash scripts/merge_fastqs.sh data out/merged $sid
-#done
+for sid in $(ls data/*.fastq.gz | cut -d"-" -f1 | sed 's:data/::' | sort | uniq)
+do
+    bash scripts/merge_fastqs.sh data out/merged $sid
+done
 
 # Run cutadapt for all merged files
-#echo "RUNNING CUTADAPT"
-#mkdir -p log/cutadapt
-#mkdir -p out/trimmed 
-#for sid in $(ls out/merged/*fastq.gz | cut -d"." -f1 | sed 's:out/merged/::' )
-#do
-#	cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/$sid.trimmed.fastq.gz  out/merged/$sid.fastq.gz > log/cutadapt/$sid.log
-#done
+echo "RUNNING CUTADAPT"
+mkdir -p log/cutadapt
+mkdir -p out/trimmed 
+for sid in $(ls out/merged/*fastq.gz | cut -d"." -f1 | sed 's:out/merged/::' )
+do
+	cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/$sid.trimmed.fastq.gz  out/merged/$sid.fastq.gz > log/cutadapt/$sid.log
+done
 #TODO: run STAR for all trimmed files
 echo "RUNNING STAR ALIGNMENT"
 for fname in out/trimmed/*.fastq.gz
