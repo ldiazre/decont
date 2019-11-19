5e82cec49ce15e1010d1f97cd0681a65248f7e1c
Laura Díaz Regalado <lauradiazregalado@gmail.com>
2019-11-19 12:45:24 +0100

    ALMOST DONE

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,13 @@
 # This script should merge all files from a given sample (the sample id is provided in the third argument)
 # into a single file, which should be stored in the output directory specified by the second argument.
 # The directory containing the samples is indicated by the first argument.
+
+
+dircontaining=$1
+outputdir=$2
+sid=$3
+
+echo "MERGING THE SAMPLES"
+mkdir -p $outputdir
+cat $dircontaining/$sid* > $outputdir/$sid.fastq.gz
+echo "MERGIND DONE FOR $sid"
