7396861f62bff043b0f80ff5311f595cccc70e76
Laura Díaz Regalado <lauradiazregalado@gmail.com>
2019-11-19 13:36:01 +0100

    FINALISED

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,18 +1,18 @@
 echo "ANLYSING HAS STARTED"
 #Download all the files specified in data/urls
-for url in $(cat data/urls) #TODO
+for url in $(cat data/urls) 
 do
     bash scripts/download.sh $url data
 done
 
 # Download the contaminants fasta file, and uncompress it
-bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
+bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes
 echo "ALL NECESSARY DATA HAVE BEEN DOWNLOADED"
 # Index the contaminants file
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 # Merge the samples into a single file
-echo "RUNING FASTQ"
+echo "RUNNING FASTQ"
 for sid in $(ls data/*.fastq.gz | cut -d"-" -f1 | sed 's:data/::' | sort | uniq)
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
@@ -34,7 +34,7 @@ do
 	sid=$(echo $(basename $fname .trimmed.fastq.gz))
 	mkdir -p out/star/$sid/
     	STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn $fname --readFilesCommand zcat --outFileNamePrefix out/star/$sid/
-	echo "ALIGNEMENT DONE FOR $sid"
+	echo "ALIGNMENT DONE FOR $sid"
 echo "ANALYSING FOR $sid HAVE FINISHED"
 done
 # TODO: create a log file containing information from cutadapt and star logs
